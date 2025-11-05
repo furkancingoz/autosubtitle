1cc59b6c433d80177e293f38c519a5d05c06e32a
furkan <efcingoz@outlook.com>
2025-11-05 20:27:55 +0300

    Initial Commit

---
+++ autosubtitle/autosubtitleApp.swift
@@ -0,0 +1,17 @@
+//
+//  autosubtitleApp.swift
+//  autosubtitle
+//
+//  Created by Furkan Cingöz on 5.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct autosubtitleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
